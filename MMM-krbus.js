2a3451f47f7e70cc2bc486b8d0d4ef97673db3ed
JeongWoojin <woojin@wapplecloud.com>
2017-02-24 12:18:05 +0900

    no message

--- MMM-krbus.js
+++ MMM-krbus.js
@@ -1,11 +1,19 @@
-var request = require('request');
+var request = require('request')
+  , parseString = request('xml2js').parseString;
 
 Module.register("krbus", {
   defaults: {
     text: "버스 도착 정보",
     ServiceKey : "",
-    stationId : "",
-    apiurl : ' http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station'
+    stations : [
+      {
+        sid : "", // Station ID
+        sname : "", // Station name
+        rid : "", // Bus route ID
+        rname : "" // Bus route name
+      }
+    ],
+    apiurl : ' http://openapi.gbis.go.kr/ws/rest/busarrivalservice'
   },
 
   getDom: function() {
@@ -14,13 +22,13 @@ Module.register("krbus", {
 
 
     if (this.config.ServiceKey === "") {
-      wrapper.innerHTML = this.name + "의 config 에서 버스 도착 정보 조회 api 사용을 위한 key를 설정하세요.";
+      wrapper.innerHTML = this.name + "의 config 에서 버스 도착 정보 조회 API 사용을 위한 key를 설정하세요.";
       wrapper.className = "dimmed light small";
       return wrapper;
     }
 
-    if (this.config.stationId === "") {
-      wrapper.innerHTML = this.name + "의 config 에서 버스정류소 아이디를 설정하세요.";
+    if (this.config.stationId === []) {
+      wrapper.innerHTML = this.name + "의 config 에서 정류소 및 버스 정보를 설정하세요";
       wrapper.className = "dimmed light small";
       return wrapper;
     }
@@ -35,8 +43,8 @@ Module.register("krbus", {
   },
 
   updateBusArrivals: function() {
-    if (this.config.ServiceKey === "" || this.config.stationId === "") {
-      Log.error("KRBUS: Need ServiceKey & stationId");
+    if (this.config.ServiceKey === "" || this.config.stations === []) {
+      Log.error("KRBUS: API 키와 정류소 및 버스 정보를 설정하세요.");
       return;
     }
 
@@ -52,11 +60,17 @@ Module.register("krbus", {
     });
   },
 
-  getParams: function() {
-    var params = "?";
+  getParams: function(sid, bid) {
+    var params = "";
 
-    params += "stationId=" + encodeURIComponent(this.config.stationId);
-    params += "&ServiceKey=" + encodeURIComponent(this.config.Servicekey);
+    if(sid === "" || bid === "") {
+      Log.error('정류소 번호, 버스 번호가 필요합니다.');
+    } else {
+      params = "?";
+      params += "stationId=" + encodeURIComponent(sid);
+      params += "&routeId=" + encodeURIComponent(bid);
+      params += "&ServiceKey=" + encodeURIComponent(this.config.Servicekey);
+    }
 
     return params;
   },
